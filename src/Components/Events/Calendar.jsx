32958763b58aae35f95cd64adce7fe0fd6a9bf80
Trevor Edwards <trevor.jeffrey.edwards@gmail.com>
2022-07-17 17:29:28 -0400

    Calendar Work

--- src/Components/Events/Calendar.jsx
+++ src/Components/Events/Calendar.jsx
@@ -1,20 +1,39 @@
 import React from 'react';
-import Calendar from 'react-calendar';
+import {Calendar, momentLocalizer} from 'react-big-calendar';
+import moment from 'moment';
+import "react-big-calendar/lib/css/react-big-calendar.css";
+
+
+const localizer = momentLocalizer(moment);
 
 const MasonCalendar = () => {
 
-  const setDate = new Date();
+  const myEventsList = [
+    {
+      start: new Date(),
+      end: new Date(),
+      title: "This is a sample event"
+    },
+    {
+      start: '07/21/2022',
+      end: '07/21/2022',
+      title: "Festivities Start at 11:00am"
+    }
+];
 
   return (
-    <div className="app">
-      <h1 className="header">Event Calendar</h1>
-      <div className="calendar-container">
-        <Calendar onChange={setDate} value={setDate}/>
-      </div>
-      <div className="text-center">
-          Selected date: {setDate.toDateString()}
-      </div>
-    </div>
+    <div class="calendar-container">
+    <Calendar
+      defaultDate={moment().toDate()}
+      defaultView="month"
+      localizer={localizer}
+      events={myEventsList}
+      resizable
+      startAccessor="start"
+      endAccessor="end"
+      style={{ height: 500 }}
+    />
+  </div>
   )
 }
 

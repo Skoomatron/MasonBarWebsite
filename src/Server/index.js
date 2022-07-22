a573d6670d183b384a9e8053488347e25b954df3
Trevor Edwards <trevor.jeffrey.edwards@gmail.com>
2022-07-22 09:09:08 -0400

    Working On Deployment Changes

--- src/Server/index.js
+++ src/Server/index.js
@@ -2,15 +2,16 @@ const router = require('./routes.js');
 const path = require('path');
 const express = require('express');
 const app = express();
+require('dotenv').config();
+
 
 app.use(express.json());
 app.use('/', router);
 app.use(express.static(path.join(__dirname, '../../dist')));
 
-const PORT = 3002;
 
-app.listen(PORT, () => {
-  console.log(`We in this port ${PORT} !`)
+app.listen(process.env.PORT, () => {
+  console.log(`We in this port ${process.env.PORT} !`)
 });
 
 module.exports = app;
